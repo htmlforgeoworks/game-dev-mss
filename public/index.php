08bdc11833928acdc1a070e14df94d2f6825517a
htmlforgeoworks <alexander.majewski77@gmail.com>
2022-05-11 16:34:16 +0000

    123456789

--- public/index.php
+++ public/index.php
@@ -5,5 +5,6 @@ echo"<p>Here will be a library of php Files</p>";
 echo"<hr>";
 echo"<h2>The following files:</h2>";
 echo"<a href='https://htmlforgeoworks.github.io/game-dev-mss/public/myphp01.php'>Myphp01.php</a>";
+echo"<br>";
 $x = "This is a php page";
-echo"This is a test and $x";
+echo"This is a test and...<br> $x";
