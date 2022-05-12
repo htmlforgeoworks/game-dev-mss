82fca926c5bf3053574f749413ff4de0dbb638fc
htmlforgeoworks <alexander.majewski77@gmail.com>
2022-05-12 16:21:13 +0000

    commit var.php

---
+++ public/variables.php
@@ -0,0 +1,10 @@
+<?php
+
+$A = "This is my first php variable";
+$a = "This is my second php variable";
+$Aa = "This is my thrid php variable";
+$aA = "This is my fourth php variable";
+$Aaa = "How many variables can we do?";
+$AAa = "Hello";
+$AAA = "How are you";
+echo $AAa;
